14960ba8581ba0efe5cc100153d99fdc405a8270
KRoLer <nikitin.dmytro@gmail.com>
2018-04-09 20:13:43 +0300

    Added persistence for Croupier singleton actor.

--- src/main/scala/com/casino/actors/Croupier.scala
+++ src/main/scala/com/casino/actors/Croupier.scala
@@ -1,6 +1,7 @@
 package com.casino.actors
 
 import akka.actor.{Actor, ActorLogging, Props}
+import akka.persistence.{PersistentActor, SnapshotOffer}
 import akka.util.Timeout
 import com.casino.actors.Croupier._
 
@@ -16,20 +17,33 @@ object Croupier {
   case class BetPlaced (balance: Double) extends CroupierResponse
   case class BetsList (bets: List[Double]) extends CroupierResponse
   case class PlayerNotFound (playerID: Long, msg: String) extends CroupierResponse
+
 }
 
-class Croupier extends Actor with ActorLogging {
+class Croupier extends PersistentActor with ActorLogging {
   implicit val timeout = Timeout(5 seconds)
+
+  override def persistenceId: String = "croupier-persistent-singleton-actor"
 
   private var state: mutable.HashMap[Long, List[(Long, Double)]] = mutable.HashMap.empty
 
-  override def receive: Receive = {
-    case PlaceBet(gameId, playerId, amount, balance) => {
-      val bets = state.getOrElse(playerId, List())
-      state.update(playerId, (gameId, amount) :: bets)
-      sender ! BetPlaced(balance)
+  def updateState(evt: PlaceBet) = {
+    val bets = state.getOrElse(evt.playerId, List())
+    state.update(evt.playerId, (evt.gameId, evt.amount) :: bets)
+  }
+
+  override def receiveCommand: Receive = {
+    case evt @ PlaceBet(gameId, playerId, amount, balance) => {
+      persist(evt){ e =>{
+        updateState(e)
+        sender ! BetPlaced(e.balance)
+      }}
     }
     case ShowBets(pid) if state.contains(pid) => sender ! BetsList(state(pid).map(_._2))
     case ShowBets(id) => sender ! PlayerNotFound(id, s"Player with id: $id not found.")
+  }
+  override def receiveRecover: Receive = {
+    case evt: PlaceBet => updateState(evt); log.info(s"Recovered $evt")
+    case SnapshotOffer(_, snapshot: mutable.HashMap[Long, List[(Long, Double)]]) => state = snapshot
   }
 }
